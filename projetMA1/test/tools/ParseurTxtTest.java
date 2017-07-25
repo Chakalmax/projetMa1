a1c9d5792a01d954eef03f3886d8d53dfb8c6001
Chakalmax <maximevanherzeele@hotmail.com>
2017-07-25 20:54:48 +0200

    ParseurTxtTest Corrected - Stable Version

--- projetMA1/test/tools/ParseurTxtTest.java
+++ projetMA1/test/tools/ParseurTxtTest.java
@@ -14,14 +14,14 @@ public class ParseurTxtTest {
 	@Test
 	public void readFileTest(){
 		String filePath = new File("").getAbsolutePath();
-		String fileName = filePath.concat("/test/ressources/test1.txt");
+		String fileName = filePath.concat("\\test\\ressources\\test1.txt");
 		KnowledgeBase kb = ParseurTxt.readFile(fileName);
 		
 		assertTrue(!kb.getDescription().equals(null));
 		assertTrue(kb.getName().equals("test1"));
 		assertEquals(kb.getSamples().size(),15);
 		assertEquals(kb.getAttributeList().size(),6);
-		assertTrue(kb.getAttributeList().get(4).isClasse());
+		assertTrue(kb.getAttributeList().get(5).isClasse());
 		assertEquals(kb.getClassAttribute().getType(),Type.Boolean);
 	}
 	
