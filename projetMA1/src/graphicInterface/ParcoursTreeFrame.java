4f6626a7e8803332bc9b8f5ae17e820dae7c2fc2
Chakalmax <maximevanherzeele@hotmail.com>
2017-08-01 10:35:58 +0200

    ParcoursTreeFrame
    It's working but still no tree

--- projetMA1/src/graphicInterface/ParcoursTreeFrame.java
+++ projetMA1/src/graphicInterface/ParcoursTreeFrame.java
@@ -1,16 +1,25 @@
 package graphicInterface;
 
+import java.awt.Component;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import DTL.DTLAlgo;
+import DTL.GainStrategy.EntropyGain;
+import DTL.GainStrategy.GainStrategy;
+import DTL.GainStrategy.GiniGain;
 import DecisionTree.DecisionTree;
 import KnowledgeBase.*;
 
@@ -21,9 +30,11 @@ public class ParcoursTreeFrame extends JFrame {
 	JPanel panel;
 	DecisionTree dT;
 	KnowledgeBase kb;
+	GainStrategy gStrat = new GiniGain();
 	
 	JPanel selectPanel;
 	JPanel treePanel;
+	JPanel boutonPanel;
 	ArrayList<JLabel> labelList;
 	ArrayList<JComponent> componentList;
 	public ParcoursTreeFrame(KnowledgeBase kb){
@@ -31,6 +42,7 @@ public class ParcoursTreeFrame extends JFrame {
 		this.panel = new JPanel();
 		//this.dT = dT; // CAREFUL: Null atm
 		this.kb = kb;
+		this.dT = DTLAlgo.DTL_algo(kb, 0, gStrat);
 		int numberAttribute = kb.getAttributeList().size();
 		this.setSize(62*numberAttribute, 200);
 	    this.setLocationRelativeTo(null);
@@ -43,12 +55,16 @@ public class ParcoursTreeFrame extends JFrame {
 		selectPanel = new JPanel();
 		addThingsSelectPanel();
 		treePanel = new TreePanel();// change it to TreePanel after.(Maybe!!)
+		boutonPanel = new JPanel();
+		addThingsBoutonPanel();
 		
-		this.getContentPane().setLayout(new GridLayout(2,1));
+		this.getContentPane().setLayout(new GridLayout(3,1));
 		this.add(treePanel);
 		this.add(selectPanel);
+		this.add(boutonPanel);
 		
 	}
+	
 	private void addThingsSelectPanel() {
 		int numberAttribute = kb.getAttributeList().size();
 		componentList = new ArrayList<JComponent>();
@@ -80,4 +96,84 @@ public class ParcoursTreeFrame extends JFrame {
 		}
 		
 	}
+	
+	private void addThingsBoutonPanel() {
+		JButton bouton = new JButton("Verifier");
+		bouton.addActionListener(new BoutonVerification());
+		this.boutonPanel.add(bouton);
+		
+	}
+	
+	private class BoutonVerification implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e){
+			Sample samp = new Sample();
+			int classIndex = kb.getIndexClass();
+			AttributeValue<?> classVal = null;
+			for(int i=0;i<componentList.size();i++){
+				if(i != classIndex){
+					if(kb.getAttributeList().get(i).getType() == TypeAttribute.Boolean){
+						JComboBox<Boolean> comp = (JComboBox<Boolean>) componentList.get(i);
+						boolean val = (boolean) comp.getSelectedItem();
+						AttributeValue<Boolean> attVal = new AttributeValue<Boolean>(val);
+						samp .add(attVal);
+					}else if(kb.getAttributeList().get(i).getType() == TypeAttribute.Nominal){
+						JComboBox<String> comp = (JComboBox<String>) componentList.get(i);
+						String val = (String) comp.getSelectedItem();
+						AttributeValue<String> attVal = new AttributeValue<String>(val);
+						samp .add(attVal);
+					}else{// == Numerical
+						JFormattedTextField comp = (JFormattedTextField) componentList.get(i);
+						float val;
+						if(comp.getText() != null && comp.getText().equals("")) 
+						    val =0;
+						else 
+						    val = Float.parseFloat(comp.getText()); 
+						AttributeValue<Float> attVal = new AttributeValue<Float>(val);
+						samp .add(attVal);
+					}
+				}else{// i == classIndex
+					if(kb.getAttributeList().get(i).getType() == TypeAttribute.Boolean){
+						JComboBox<Boolean> comp = (JComboBox<Boolean>) componentList.get(i);
+						boolean val = (boolean) comp.getSelectedItem();
+						classVal = new AttributeValue<Boolean>(val);
+					}else if(kb.getAttributeList().get(i).getType() == TypeAttribute.Nominal){
+						JComboBox<String> comp = (JComboBox<String>) componentList.get(i);
+						String val = (String) comp.getSelectedItem();
+						classVal = new AttributeValue<String>(val);
+					}else{// == Numerical
+						JFormattedTextField comp = (JFormattedTextField) componentList.get(i);
+						float val;
+						if(comp.getText() != null && comp.getText().equals("")) 
+						    val =0;
+						else 
+						    val = Float.parseFloat(comp.getText()); 
+						classVal = new AttributeValue<Float>(val);
+					}
+					
+				}
+			}
+			AttributeValue<?> decision = dT.getDecision(samp);
+			System.out.println(classVal);
+			System.out.println(decision);
+			if(decision.equals(classVal))
+				showCorrectAnswer(decision, classVal);
+			else
+				showIncorrectAnswer(decision, classVal);
+		}
+
+		private void showIncorrectAnswer(AttributeValue<?> decision, AttributeValue<?> classVal) {
+			JOptionPane jopt = new JOptionPane();
+			jopt.showMessageDialog(null, "Vous avez répondu : " + classVal + "\n" + "mais la réponse correcte est : "+ decision,"Detail",JOptionPane.INFORMATION_MESSAGE);
+			
+		}
+
+		private void showCorrectAnswer(AttributeValue<?> decision, AttributeValue<?> classVal) {
+			JOptionPane jopt = new JOptionPane();
+			jopt.showMessageDialog(null, "Vous avez répondu : " + classVal + "\n" + "et c'est correct","Detail" , JOptionPane.INFORMATION_MESSAGE);
+			
+		}
+	}
+
+
 }
